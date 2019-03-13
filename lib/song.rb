8ddca04e460cad544e4cec5e783a9b1780df8256
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-13 21:43:47 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+  @@artists = []
+  @@count = 0
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+    @@artists << artist
+    @@count += 1 
+    @@genres << genre 
+  end
+  
+end 
